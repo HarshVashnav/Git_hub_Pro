65a8aef701ce9363f6097908428a653f72a24a18
HarshVaishnav <harshrvaishnav@gmail.com>
2022-09-14 11:48:11 +0530

    new feature added to be pushed

---
+++ Repository/src/New_feature_push.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashMap;
+
+public class New_feature_push {
+
+	public static void main(String[] args) {
+
+		LinkedHashMap<String, String> lhm = new LinkedHashMap<String, String>();
+		lhm.put("100", "ha");
+		lhm.put("101", "am");
+		lhm.put("102", "sa");
+
+		System.out.println(lhm);
+	}
+
+}
